a7e6f56cbb6494fb41b59efb921680ed0510fd6c
jmday <jamison.day@holo.host>
2019-04-22 18:54:48 -0600

    init commit of SrcChain-HelloMe

--- test/index.js
+++ test/index.js
@@ -3,21 +3,39 @@
 const { Config, Scenario } = require("@holochain/holochain-nodejs")
 Scenario.setTape(require("tape"))
 
+// Create a reference to our DNA microservice
 const dnaPath = "./dist/core-concepts-examples.dna.json"
 const dna = Config.dna(dnaPath)
-
+// Create an agent, alice, within our DNA microservice
 const agentAlice = Config.agent("alice")
 const instanceAlice = Config.instance(agentAlice, dna)
+// Create an agent, bob, within our DNA microservice
+const agentBob = Config.agent("bob")
+const instanceBob = Config.instance(agentBob, dna)
+// Define our scenario with alice and bob
+const scenario = new Scenario([instanceAlice, instanceBob])
 
-const scenario = new Scenario([instanceAlice])
+scenario.runTape("post & get a private message", async (t, { alice, bob }) => {
 
-scenario.runTape("description of example test", async (t, { alice }) => {
+  // A private message
+  const message_string = "Hello, Alice, but not Bob!"
+  const message = {"content": message_string};
 
-  // Make a call to a Zome function
-  // Call get_message within message_zome, pass no parameters
-  const result = alice.call("message_zome", "get_message", {})
-  console.log("Result:", result)
+  // Alice creates a private message
+  const addr = alice.call("message_zome", "create_message", { message_string })
+  console.log("Alice's Message Address:", addr)
 
-  // check for equality of the actual and expected results
-  t.deepEqual(result, { Ok: "Hello, Holo!" })
+  // Bob can't retrieve the message since it is not in the DHT
+  const resultBob = bob.call("message_zome", "get_message", {"address": addr.Ok})
+  console.log("Bob's Result:", resultBob)
+  
+  // Alice can retrieve the message since it is in her source chain
+  const resultAlice = alice.call("message_zome", "get_message", {"address": addr.Ok})
+  console.log("Alice's Result:", resultAlice)
+
+  // check for Alice's equality of the actual and expected results
+  t.deepEqual(resultAlice, { Ok: message })
+  // check that Bob get's the proper error
+  t.deepEqual(resultBob, { Err: { Internal: 'No entry at this address' } })
+
 })
--- zomes/message_zome/code/src/lib.rs
+++ zomes/message_zome/code/src/lib.rs
@@ -9,35 +9,73 @@ extern crate serde_json;
 extern crate holochain_core_types_derive;
 
 use hdk::{
+    entry_definition::ValidatingEntryType,
     error::ZomeApiResult,
 };
 use hdk::holochain_core_types::{
+    cas::content::Address,
+    entry::Entry,
+    dna::entry_types::Sharing,
     error::HolochainError,
-    json::JsonString,
+    json::JsonString
 };
 
 // see https://developer.holochain.org/api/0.0.12-alpha1/hdk/ for info on using the hdk library
 
-pub fn handle_get_message() -> ZomeApiResult<String> {
-    Ok(String::from("Hello, Holo!"))
+#[derive(Serialize, Deserialize, Debug, DefaultJson, Clone)]
+pub struct MessageEntry {
+    content: String,
+}
+
+fn message_entry_definition() -> ValidatingEntryType {
+    entry!(
+        name: "message_entry",
+        description: "A *PRIVATE* message entry",
+        sharing: Sharing::Private,
+        validation_package: || {
+            hdk::ValidationPackageDefinition::Entry
+        },
+
+        validation: | _validation_data: hdk::EntryValidationData<MessageEntry>| {
+            Ok(())
+        }
+    )
+}
+
+pub fn handle_create_message(message_string: String) -> ZomeApiResult<Address> {
+    let entry = Entry::App("message_entry".into(), MessageEntry { content: message_string }.into());
+    let address = hdk::commit_entry(&entry)?;
+
+    Ok(address)
+}
+
+pub fn handle_get_message(address: Address) -> ZomeApiResult<MessageEntry> {
+    hdk::utils::get_as_type(address)
 }
 
 define_zome! {
     entries: [
+        message_entry_definition()
     ]
 
     genesis: || { Ok(()) }
 
     functions: [
+        create_message: {
+            inputs: |message_string: String|,
+            outputs: |result: ZomeApiResult<Address>|,
+            handler: handle_create_message
+        }
         get_message: {
-            inputs: | |,
-            outputs: |result: ZomeApiResult<String>|,
+            inputs: |address: Address|,
+            outputs: |result: ZomeApiResult<MessageEntry>|,
             handler: handle_get_message
         }
     ]
 
     traits: {
         hc_public [
+            create_message,
             get_message
         ]
     }
